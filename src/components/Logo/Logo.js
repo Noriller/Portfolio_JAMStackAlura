cde5d0a8adfecb822504c21188c48de4f9dc6131
Bruno Noriller <brunonoriller@gmail.com>
2021-02-17 19:58:43 -0300

    logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,10 +3,17 @@ import styled from 'styled-components';
 
 const LogoContainer = styled.div`
   border: 1px solid;
-  background-color: red;
   border-radius: 50%;
   height: ${({ theme }) => theme.getBreakpointValue(theme.baseSizes.medium.xs, theme.baseSizes.medium.md, theme.baseSizes.medium.lg, theme.baseSizes.medium.xl)};
   width: ${({ theme }) => theme.getBreakpointValue(theme.baseSizes.medium.xs, theme.baseSizes.medium.md, theme.baseSizes.medium.lg, theme.baseSizes.medium.xl)};
+  overflow: hidden;
+  position: relative;
+  display: inline-block;
+  background-image: url("/BrunoHope.jpg");
+  background-repeat: no-repeat;
+  background-position: 50%;
+  background-size: 105%;
+  padding: 1.5em
 `;
 
 export default function Logo() {
@@ -14,7 +21,3 @@ export default function Logo() {
     <LogoContainer />
   );
 }
-
-// ${({ theme }) => theme.getBreakpointValue(theme.textSize.display, theme.textSize.title1, '')}
-// ${insertMediaQuery(xs, md, xl)}
-// import styled, { css } from 'styled-components';
