524e0b1ca1fd6a83c29f8c548b26c4593b281d68
Bruno Noriller <brunonoriller@gmail.com>
2021-02-17 20:37:37 -0300

    meta og tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,15 @@ export default function App({ Component, pageProps }) {
     <ThemeWrapper>
       <Head>
         <title>My Portfolio</title>
+        <meta property="og:title" content="The Awesome Portfolio of Bruno Noriller" key="title" />
+        <meta property="og:url" content="http://noriller.github.io/" key="url" />
+        <meta
+          property="og:description"
+          content="Chekout everything I have in this URL. Also, this image was photoshopped by me... indeed a programmer of many talents."
+          key="description"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/BrunoHope.jpg" />
       </Head>
       <Component {...pageProps} />
     </ThemeWrapper>
