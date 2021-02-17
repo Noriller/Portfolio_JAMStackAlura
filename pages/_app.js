be5305cbaaeed92d99d2fe544182895344663269
Bruno Noriller <brunonoriller@gmail.com>
2021-02-17 20:41:48 -0300

    og tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,15 +9,14 @@ export default function App({ Component, pageProps }) {
     <ThemeWrapper>
       <Head>
         <title>My Portfolio</title>
-        <meta property="og:title" content="The Awesome Portfolio of Bruno Noriller" key="title" />
-        <meta property="og:url" content="http://noriller.github.io/" key="url" />
+        <meta property="og:title" content="The Awesome Portfolio of Bruno Noriller" />
+        <meta property="og:url" content="http://noriller.github.io/" />
         <meta
           property="og:description"
           content="Chekout everything I have in this URL. Also, this image was photoshopped by me... indeed a programmer of many talents."
-          key="description"
         />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/BrunoHope.jpg" />
+        <meta property="og:image" content="https://bruno-noriller-jamstack-alura.vercel.app/BrunoHope.jpg" />
       </Head>
       <Component {...pageProps} />
     </ThemeWrapper>
